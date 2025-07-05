10d28fa03fcfbd2daf95378d8ac44b183f7d1953
acarrera10 <alejandro@trescientosuno.com.ar>
2025-07-04 22:54:16 -0300

    Add files via upload

---
+++ service-worker.js
@@ -0,0 +1,34 @@
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open('ruleta-cache').then(cache =>
+      cache.addAll([
+        './',
+        './index.html',
+        './style.css',
+        './script.js',
+        './manifest.json',
+        './service-worker.js',
+        './Video Trivias fondo-landscape.mp4',
+        './Video Trivias fondo-vertical.mp4',
+        './wheel_pointer_ok.png',
+        './cordobesa.webp',
+        './participar.webp',
+        './taca-taca.webp',
+        './otro-tiro.webp',
+        './disfrutando.webp',
+        './champaqui.webp',
+        './bancor.webp',
+        './cordoba.webp',
+        './punteada.webp',
+        './icon-192.png',
+        './icon-512.png'
+      ])
+    )
+  );
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request).then(resp => resp || fetch(event.request))
+  );
+});
